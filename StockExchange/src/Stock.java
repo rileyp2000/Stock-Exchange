759ce7a2365d3b3ea6421d7e865b9bbe58eed196
rileyp2000 <rileyp@bishopireton.org>
2017-10-25 13:44:39 -0400

    sharetrade

---
+++ StockExchange/src/Stock.java
@@ -0,0 +1,16 @@
+/**
+ * <p></p> 
+ * Oct 25, 2017
+ * @author Patrick Riley
+ */
+
+/**
+ * @author rileyp
+ *
+ */
+public class Stock {
+	private String symbol, name;
+	private double price;
+	
+	
+}
