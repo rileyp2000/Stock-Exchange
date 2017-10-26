1f64fdeac27a8e5100694c94ec23cf8b7c4f16bd
rileyp2000 <rileyp@bishopireton.org>
2017-10-26 14:57:49 -0400

    working on brokerage

--- StockExchange/src/Brokerage.java
+++ StockExchange/src/Brokerage.java
@@ -13,19 +13,51 @@ import java.util.TreeSet;
  */
 public class Brokerage implements Login{
 	
-	private TreeMap registeredTrader;
-	private TreeSet loggedInTraders;
+	private TreeMap<String, Trader> registeredTrader;
+	private TreeSet<Trader> loggedInTraders;
 	
-	@Override
+	public Brokerage() {
+		registeredTrader = new TreeMap<String, Trader>();
+		loggedInTraders = new TreeSet<Trader>();
+	}
+	
+	/**
+	   * Tries to register a new user with a given screen name
+	   * and password;
+	   * @param name the screen name of the user.
+	   * @param password the password for the user.
+	   * @return 0 if successful, or an error code (a negative integer) if failed:<br>
+	   * -1 -- invalid screen name (must be 4-10 chars)<br>
+	   * -2 -- invalid password (must be 2-10 chars)<br>
+	   * -3 -- the screen name is already taken.
+	   */
 	public int addUser(String name, String password) {
-		// TODO Auto-generated method stub
+		int len = name.length();
+		if(len < 4 || len > 10)
+			return -1;
+		len = password.length();
+		if(len < 2 || len > 10)
+			return -2;
+		if(registeredTrader.containsKey(name))
+			return -3;
+		registeredTrader.put(name, new Trader(this, name, password));
 		return 0;
 	}
 
-	@Override
+	  /**
+	   * Tries to login a user with a given screen name and password;
+	   * @param name the screen name of the user.
+	   * @param password the password for the user.
+	   * @return 0 if successful, or an error code (a negative integer) if failed:<br>
+	   * -1 -- screen name not found<br>
+	   * -2 -- invalid password<br>
+	   * -3 -- user is already logged in.
+	   */
 	public int login(String name, String password) {
-		// TODO Auto-generated method stub
-		return 0;
+		Trader t = registeredTrader.get(name);
+		if(t == null) return -1;
+		String storedPassword = t.getPassword();
+		
 	}
 
 }
