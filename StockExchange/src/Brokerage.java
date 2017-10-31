e8df85f17c544a424fb3753e0e0cd8854db77daa
rileyp2000 <rileyp@bishopireton.org>
2017-10-31 09:37:01 -0400

    finished login logout and some of trader

--- StockExchange/src/Brokerage.java
+++ StockExchange/src/Brokerage.java
@@ -11,53 +11,84 @@ import java.util.TreeSet;
  * @author rileyp
  *
  */
-public class Brokerage implements Login{
-	
+public class Brokerage implements Login {
+
 	private TreeMap<String, Trader> registeredTrader;
 	private TreeSet<Trader> loggedInTraders;
-	
+
 	public Brokerage() {
 		registeredTrader = new TreeMap<String, Trader>();
 		loggedInTraders = new TreeSet<Trader>();
 	}
-	
+
 	/**
-	   * Tries to register a new user with a given screen name
-	   * and password;
-	   * @param name the screen name of the user.
-	   * @param password the password for the user.
-	   * @return 0 if successful, or an error code (a negative integer) if failed:<br>
-	   * -1 -- invalid screen name (must be 4-10 chars)<br>
-	   * -2 -- invalid password (must be 2-10 chars)<br>
-	   * -3 -- the screen name is already taken.
-	   */
+	 * Tries to register a new user with a given screen name and password;
+	 * 
+	 * @param name
+	 *            the screen name of the user.
+	 * @param password
+	 *            the password for the user.
+	 * @return 0 if successful, or an error code (a negative integer) if failed:<br>
+	 *         -1 -- invalid screen name (must be 4-10 chars)<br>
+	 *         -2 -- invalid password (must be 2-10 chars)<br>
+	 *         -3 -- the screen name is already taken.
+	 */
 	public int addUser(String name, String password) {
 		int len = name.length();
-		if(len < 4 || len > 10)
+		if (len < 4 || len > 10)
 			return -1;
 		len = password.length();
-		if(len < 2 || len > 10)
+		if (len < 2 || len > 10)
 			return -2;
-		if(registeredTrader.containsKey(name))
+		if (registeredTrader.containsKey(name))
 			return -3;
 		registeredTrader.put(name, new Trader(this, name, password));
 		return 0;
 	}
 
-	  /**
-	   * Tries to login a user with a given screen name and password;
-	   * @param name the screen name of the user.
-	   * @param password the password for the user.
-	   * @return 0 if successful, or an error code (a negative integer) if failed:<br>
-	   * -1 -- screen name not found<br>
-	   * -2 -- invalid password<br>
-	   * -3 -- user is already logged in.
-	   */
+	/**
+	 * Tries to login a user with a given screen name and password;
+	 * 
+	 * @param name
+	 *            the screen name of the user.
+	 * @param password
+	 *            the password for the user.
+	 * @return 0 if successful, or an error code (a negative integer) if failed:<br>
+	 *         -1 -- screen name not found<br>
+	 *         -2 -- invalid password<br>
+	 *         -3 -- user is already logged in.
+	 */
 	public int login(String name, String password) {
 		Trader t = registeredTrader.get(name);
-		if(t == null) return -1;
+		if (t == null)
+			return -1;
+		if (loggedInTraders.contains(t))
+			return -3;
 		String storedPassword = t.getPassword();
-		
+		if (storedPassword.equals(password))
+			return 0;
+		else
+			return -2;
+
+	}
+	
+	/**
+	 * <p>Logs out the Trader</p>
+	 * 
+	 * @param t Trader to be removed
+	 *
+	 *void
+	 */
+	public void logout(Trader t) {
+		loggedInTraders.remove(t);
+	}
+	
+	public void getQuote(String symbol, Trader trader) {
+		//TODO inlude receiveMessage for trader
+	}
+	
+	placeOrder(TradeOrder order){
+		//TODO
 	}
 
 }
--- StockExchange/src/Trader.java
+++ StockExchange/src/Trader.java
@@ -1,21 +1,56 @@
 
-public class Trader {
+public class Trader implements Comparable<Trader> {
 	private Brokerage brokerage;
 	private String username, password;
-	
+
 	public Trader(Brokerage b, String un, String pwd) {
-		brokerage = b; 
+		brokerage = b;
 		username = un;
 		password = pwd;
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * @return the username
+	 */
+	public String getName() {
+		return username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	
+	
+	public void getQuote(String symbol) {
+		brokerage.getQuote(symbol, this);
+	}
+
+	@Override
+	public int compareTo(Trader other) {
+		String n1 = this.getName().toLowerCase();
+		String n2 = other.getName().toLowerCase();
+		return n1.compareTo(n2);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Trader)
+			return compareTo(((Trader) other)) == 0;
+		else
+			return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
 		return "Trader Brokerage: " + brokerage + ", Username: " + username;
 	}
-	
-	
+
 }
