ee69ff02cc840dbc25ac86301570431e7f1c4f48
rileyp2000 <rileyp@bishopireton.org>
2017-11-03 11:01:53 -0400

    asd

--- StockExchange/src/StockExchange.java
+++ StockExchange/src/StockExchange.java
@@ -12,49 +12,59 @@ import java.util.HashMap;
  */
 public class StockExchange {
 	private HashMap<String, Stock> stocks;
-	
+
 	public StockExchange() {
 		stocks = new HashMap<String, Stock>();
 	}
-	
+
 	/**
 	 * 
 	 * Adds a new stock with given parameters to the listed stocks
 	 * 
-	 * @param symbol stock symbol
-	 * @param name full name
-	 * @param price opening price
+	 * @param symbol
+	 *            stock symbol
+	 * @param name
+	 *            full name
+	 * @param price
+	 *            opening price
 	 *
-	 *void
+	 *            void
 	 */
-	public void listStock(String symbol,String name,double price) {
+	public void listStock(String symbol, String name, double price) {
 		stocks.put(symbol, new Stock(symbol, name, price));
 	}
-	
+
 	/**
 	 * 
 	 * Returns a quote for a given stock.
 	 * 
-	 * @param symbol stock symbol
+	 * @param symbol
+	 *            stock symbol
 	 * @return
 	 *
-	 *java.lang.String a text message that contains the quote.
+	 * 		java.lang.String a text message that contains the quote.
 	 */
-	public java.lang.String getQuote(String symbol){
-		return stocks.get(symbol).getQuote();
+	public String getQuote(String symbol) {
+		if (stocks.get(symbol) != null)
+			return stocks.get(symbol).getQuote();
+		else
+			throw new IllegalArgumentException("Stock don't exist boi");
 	}
-	
+
 	/**
-	 * Places a trade order by calling stock.placeOrder for the stock specified by the stock symbol in the trade order.
-	 * @param order a trading order to be placed with this stock exchange.
+	 * Places a trade order by calling stock.placeOrder for the stock specified by
+	 * the stock symbol in the trade order.
+	 * 
+	 * @param order
+	 *            a trading order to be placed with this stock exchange.
 	 *
-	 *void
+	 *            void
 	 */
 	public void placeOrder(TradeOrder order) {
-		if(stocks.get(order.getSymbol()) != null)
+		if (stocks.get(order.getSymbol()) != null)
 			stocks.get(order.getSymbol()).placeOrder(order);
-			else
-				throw new IllegalArgumentException("Stock dont exist boi");
-		
+		else
+			throw new IllegalArgumentException("Stock don't exist boi");
+
 	}
 }
